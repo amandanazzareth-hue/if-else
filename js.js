c0335ec8953510c5e4ba3fe98fdbc90461670538
amandanazzareth-hue <amandanazzareth@gmail.com>
2026-06-10 16:45:23 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+const idade = 18
+let idadeInformada
+
+idadeInformada = prompt("Informe sua idade:")
+
+if(idade <= idadeInformada){
+    alert("Você maior de idade!")
+
+}else{
+    alert("Você é menor de idade!")
+}
